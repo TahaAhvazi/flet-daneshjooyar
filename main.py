a82aa10ad819039c3bc0e71ea722ff1a25e28fc8
Taha Ahvazi <tahax.dev@gmail.com>
2024-01-17 14:48:38 +0330

    Learn about Checkbox

--- main.py
+++ main.py
@@ -1,30 +1,18 @@
 import flet as ft
-import time
 
 
 def main(page: ft.Page):
-    page.title = "Daneshjooyar conter example"
-    page.vertical_alignment = ft.MainAxisAlignment.CENTER
 
-    def button_clicked(input):
-        greetings.controls.append(
-            ft.Text(f"Hello {first_name.value} {last_name.value}"))
-        first_name.value = ""
-        last_name.value = ""
+    def checkbox_change(e):
+        mytext.value = (
+            f"You have done Learn flet : {myCheckBox.value}"
+        )
         page.update()
-        first_name.focus()
 
-    first_name = ft.TextField(label="First Name", autofocus=True)
-    last_name = ft.TextField(label="Last Name")
-    submit_button = ft.ElevatedButton("Say Hello", on_click=button_clicked)
-    greetings = ft.Column()
-
-    page.add(
-        first_name,
-        last_name,
-        submit_button,
-        greetings,
-    )
+    mytext = ft.Text()
+    myCheckBox = ft.Checkbox(
+        label="Learn flet with daneshjooyar", value=False, on_change=checkbox_change)
+    page.add(mytext, myCheckBox)
 
 
 ft.app(target=main)
