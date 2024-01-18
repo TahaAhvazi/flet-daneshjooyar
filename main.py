f1def0580336dc27dc4c2770197b19d777f6ee51
Taha Ahvazi <tahax.dev@gmail.com>
2024-01-18 18:17:25 +0330

    GridView & Listview

--- main.py
+++ main.py
@@ -2,17 +2,23 @@ import flet as ft
 
 
 def main(page: ft.Page):
+    myGridView = ft.GridView(expand=True, max_extent=120, aspect_ratio=0.5)
+    page.add(myGridView)
 
-    def checkbox_change(e):
-        mytext.value = (
-            f"You have done Learn flet : {myCheckBox.value}"
+    for i in range(5000):
+        myGridView.controls.append(
+            ft.Container(
+                ft.Text(f"Item{i+1}", color=ft.colors.BLACK),
+                width=100,
+                height=100,
+                alignment=ft.alignment.center,
+                bgcolor=ft.colors.AMBER_300,
+                border_radius=ft.border_radius.all(8)
+            )
+
         )
-        page.update()
 
-    mytext = ft.Text()
-    myCheckBox = ft.Checkbox(
-        label="Learn flet with daneshjooyar", value=False, on_change=checkbox_change)
-    page.add(mytext, myCheckBox)
+    page.update()
 
 
 ft.app(target=main)
