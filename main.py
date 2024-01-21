8216bc9e9e4f395e21b68bda99113b157bf09e86
Taha Ahvazi <tahax.dev@gmail.com>
2024-01-21 19:28:41 +0330

    Work with user Controllers

--- main.py
+++ main.py
@@ -1,29 +1,45 @@
 import flet as ft
-import time
+
+
+class GreeterController(ft.UserControl):
+    def build(Self):
+        return ft.Text("Welcome!")
+
+
+class LoginApproachController(ft.UserControl):
+    def build(self):
+        return ft.Column([
+            ft.TextField(label="Username", width=300),
+            ft.ElevatedButton("Login"),
+        ]
+        )
+
+
+class CounterController(ft.UserControl):
+    def add_number(self, event):
+        self.counter += 1
+        self.text.value = str(self.counter)
+        self.update()
+
+    def decrement_number(self, event):
+        self.counter -= 2
+        self.text.value = str(self.counter)
+        self.update()
+
+    def build(self):
+        self.counter = 0
+        self.text = ft.Text(str(self.counter))
+        return ft.Row([ft.ElevatedButton("REMOVE", on_click=self.decrement_number), self.text, ft.ElevatedButton("ADD", on_click=self.add_number)], vertical_alignment=ft.MainAxisAlignment.CENTER)
 
 
 def main(page: ft.Page):
-    container = ft.Container(
-        width=150,
-        height=150,
-        bgcolor="red",
-        border_radius=13,
-        animate=ft.animation.Animation(1000, ft.AnimationCurve.BOUNCE_OUT)
-    )
-
-    def animate_container(self):
-        container.width = 100 if container.width == 150 else 150
-        container.height = 50 if container.height == 150 else 150
-        container.bgcolor = "blue" if container.bgcolor == "red" else "red"
-        container.border_radius = 30 if container.border_radius == 13 else 13
-        container.update()
-    elevatedBuutton = ft.ElevatedButton(
-        "Animate Container!",
-        on_click=animate_container
-    )
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
-    page.add(container, elevatedBuutton)
+    page.add(
+        GreeterController(),
+        LoginApproachController(),
+        CounterController(),
+    )
 
 
 ft.app(target=main)
