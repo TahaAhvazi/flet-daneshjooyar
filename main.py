9856b15735124c3939cffe1f24693a6a9c26204f
Taha Ahvazi <tahax.dev@gmail.com>
2024-01-21 19:56:27 +0330

    Work with image slider

--- main.py
+++ main.py
@@ -1,45 +1,35 @@
 import flet as ft
 
 
-class GreeterController(ft.UserControl):
-    def build(Self):
-        return ft.Text("Welcome!")
-
-
-class LoginApproachController(ft.UserControl):
-    def build(self):
-        return ft.Column([
-            ft.TextField(label="Username", width=300),
-            ft.ElevatedButton("Login"),
-        ]
-        )
-
-
-class CounterController(ft.UserControl):
-    def add_number(self, event):
-        self.counter += 1
-        self.text.value = str(self.counter)
-        self.update()
-
-    def decrement_number(self, event):
-        self.counter -= 2
-        self.text.value = str(self.counter)
-        self.update()
-
-    def build(self):
-        self.counter = 0
-        self.text = ft.Text(str(self.counter))
-        return ft.Row([ft.ElevatedButton("REMOVE", on_click=self.decrement_number), self.text, ft.ElevatedButton("ADD", on_click=self.add_number)], vertical_alignment=ft.MainAxisAlignment.CENTER)
-
-
 def main(page: ft.Page):
+    page.title = "Image Slider!"
+    page.theme_mode = ft.ThemeMode.LIGHT
+    page.padding = 30
+    page.update()
+
+    img = ft.Image(
+        src=f"https://picsum.photos/200/200",
+        width=100,
+        height=100,
+        fit=ft.ImageFit.CONTAIN,
+    )
+    images = ft.Row(expand=1, wrap=False, scroll="always")
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
-    page.add(
-        GreeterController(),
-        LoginApproachController(),
-        CounterController(),
-    )
+    page.add(img, images)
+
+    for i in range(0, 30):
+        images.controls.append(
+            ft.Image(
+                f"https://picsum.photos/200/200?{i}",
+                width=200,
+                height=200,
+                fit=ft.ImageFit.NONE,
+                repeat=ft.ImageRepeat.NO_REPEAT,
+                border_radius=ft.border_radius.all(13.0)
+            )
+        )
+    page.update()
 
 
 ft.app(target=main)
